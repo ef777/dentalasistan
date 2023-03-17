57bec282cf875d46737455181488c282906e8e34
ef777 <efn444@gmail.com>
2023-03-17 19:45:22 +0300

    v20

--- lib/randevu/randevutarih.dart
+++ lib/randevu/randevutarih.dart
@@ -8,9 +8,6 @@ class AppointmentPage extends StatefulWidget {
 
 class _AppointmentPageState extends State<AppointmentPage> {
   DateTime _selectedDate = DateTime.now();
-  DateTime _selectedStartDate = DateTime.now();
-  DateTime _selectedEndDate = DateTime.now();
-
   List<String> _availableHours = [
     '9:00 AM',
     '10:00 AM',
@@ -64,23 +61,6 @@ class _AppointmentPageState extends State<AppointmentPage> {
     );
   }
 
-  Future<void> _selectDateRange() async {
-    final picked = await showDateRangePicker(
-      context: context,
-      firstDate: DateTime.now(),
-      lastDate: DateTime.now().add(Duration(days: 365)),
-      helpText: 'Lütfen tatil tarih aralığını seçin:',
-      cancelText: 'İptal',
-      confirmText: 'Tamam',
-    );
-    if (picked != null) {
-      setState(() {
-        _selectedStartDate = picked.start;
-        _selectedEndDate = picked.end;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -118,23 +98,18 @@ class _AppointmentPageState extends State<AppointmentPage> {
                 ),
                 TextButton(
                   onPressed: () async {
-                    final Future<void> picked = _selectDateRange();
-
-                    /*     final DateTime? picked = await showDatePicker(
+                    final DateTime? picked = await showDatePicker(
                       //  initialEntryMode
                       context: context,
-
-                      selectableDayPredicate: (DateTime val) =>
-                          val.weekday == 5 || val.weekday == 6 ? false : true,
                       initialDate: _selectedDate,
                       firstDate: DateTime.now(),
                       lastDate: DateTime(DateTime.now().year + 1),
-                    ); */
-                    /*  if (picked != null && picked != _selectedDate) {
+                    );
+                    if (picked != null && picked != _selectedDate) {
                       setState(() {
                         _selectedDate = picked;
                       });
-                    } */
+                    }
                   },
                   child: Text(
                     DateFormat.yMMMMEEEEd().format(_selectedDate),
